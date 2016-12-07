84997277082de621c34e0b498b40424450aa9e94
landesko <dale2@umbc.edu>
2016-12-06 21:22:52 -0500

    created class instance

--- bizCardOCR.py
+++ bizCardOCR.py
@@ -13,7 +13,8 @@ def readCard(bizCard):
 
 	return lines
 
-def getName(emailAddress, data):
+#Checks to see if name is found anywhere in the email name, otherwise returns "Not Found"
+def findName(emailAddress, data):
 
 	sep = '@'
 
@@ -26,32 +27,50 @@ def getName(emailAddress, data):
 		#print(words)
 
 		for word in words:
-			if word.lower() in emailName:
+			if word.lower() in emailName.lower():
 				if line != emailAddress:
 					name = line
 					return name
 
-def getPhoneNumber(data):
+	return ("Not Found")
+
+#Returns the first phone number found matching regex, otherwise returns "Not Found"
+def findPhoneNumber(data):
 	# http://stackoverflow.com/questions/3868753/find-phone-numbers-in-python-script
 	phonePattern = re.compile(".*(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})")
 
 	for line in data:
 		if phonePattern.match(line):
 			phoneNumber = line
+			phoneNumber = re.sub("\D", "", phoneNumber)
+			return phoneNumber
 
-	phoneNumber = re.sub("\D", "", phoneNumber)
-
-	return phoneNumber
-
-def getEmailAddress(data):
+	return ("Not Found")
+	
+#Returns email address matching regex, otherwise returns "Not Found"
+def findEmailAddress(data):
 	# http://emailregex.com/
 	emailPattern = re.compile("(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
 
 	for line in data:
 		if emailPattern.match(line):
 			emailAddress = line
+			return emailAddress
 
-	return emailAddress
+	return ("Not Found")
+
+class Contact:
+	def __init__(self, name, phoneNumber, emailAddress):
+		self.name = name
+		self.phoneNumber = phoneNumber
+		self.emailAddress = emailAddress
+
+	def __str__(self):
+		return ("Name: %s\nPhone: %s\nEmail: %s" % (name, phoneNumber, emailAddress))
+		
+	
+
+
 
 if __name__=="__main__":
 
@@ -61,12 +80,14 @@ if __name__=="__main__":
 
 	#print(data)
 
-	phoneNumber = getPhoneNumber(data)
-	emailAddress = getEmailAddress(data)
-	name = getName(emailAddress, data)
+	phoneNumber = findPhoneNumber(data)
+	emailAddress = findEmailAddress(data)
+	name = findName(emailAddress, data)
 
-	print("Name: %s" % name)
-	print("Phone: %s" % phoneNumber)
-	print("Email: %s" % emailAddress)
+	#print("Name: %s" % name)
+	#print("Phone: %s" % phoneNumber)
+	contact1 = Contact(name, phoneNumber, emailAddress)
+
+	print(contact1)
 
 	
